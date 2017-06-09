23f3599dff27035b6a0bab60a76e609ba409161b
狂奔的扫把星 <z@zhaoyi.me>
2017-06-09 18:04:34 +0800

    updating.

--- src/lib/fonts/fonts.js
+++ src/lib/fonts/fonts.js
@@ -2,7 +2,7 @@
 class stream {
     constructor ( buffer ) {
         this.__buffer = buffer;
-        this.__pointer = 0;
+        this.__pointer = 0;     // byte
         this.__reader = new DataView( this.__buffer );
     }
 
@@ -18,9 +18,38 @@ class stream {
         return _t;
     }
 
+    getInt16() {
+        var _t = this.__reader.getInt16( this.___pointer, false );
+        this.__pointer += 2;
+        return _t;
+    }
+
+    getFixed32() {
+        var _t = this.__reader.getUint32( this.__pointer, false );
+        _t = ( _t >> 16 ) + ( _t & 0xFFFF ) / 0x10000;
+        this.__pointer += 4;
+        return _t;
+    }
+
     getUint8Array( length ) {
         var _t = new Uint8Array( this.__buffer, this.__pointer, length );
         this.__pointer += length;
+        return _t;
+    }
+
+    getUint16Array( length ) {
+        var _t = new Uint16Array( length );
+        for ( let i = 0; i < length; i++, this.__pointer += 2 ) {
+            _t[i] = this.__reader.getUint16( this.__pointer, false );
+        }
+        return _t;
+    }
+
+    getInt16Array( length ) {
+        var _t = new Int16Array( length );
+        for ( let i = 0; i < length; i++, this.__pointer += 2 ) {
+            _t[i] = this.__reader.getInt16( this.___pointer, false );
+        }
         return _t;
     }
 
@@ -43,9 +72,185 @@ class stream {
         return _r;
     }
 
+    getUint16ArrayToEnd () {
+        var _last = ( this.__buffer.byteLength - this.__pointer ) >> 1;
+        var _t = new Uint16Array( _last );
+        for ( let i = 0; i < _last; i++, this.__pointer += 2 ) {
+            _t[i] = this.__reader.getUint16( this.__pointer, false );
+        }
+        return _t;
+    }
+
     slice ( begin, end ) {
         var _buff = this.__buffer.slice( begin, end );
         return new stream( _buff );
+    }
+}
+
+class sfnt {
+    constructor ( stm ) {
+        this.stream = stm;
+        this.parser();
+    }
+}
+
+class cmap extends sfnt {
+    // pointer has been set.
+    _sequentialMap ( length ) {
+        var _vals = [];
+
+        for ( let i = 0; i < length; i++ ) {
+            var _t = {};
+            _t.startCharCode = this.stream.getUint32();
+            _t.endCharCode = this.stream.getUint32();
+            _t.startGlyphID = this.stream.getUint32();
+
+            _vals.push( _t );
+        }
+
+        return _vals;
+    }
+
+    // pointer has been set.
+    _constantMap ( length ) {
+        var _vals = [];
+
+        for ( let i = 0; i < length; i++ ) {
+            var _t = {};
+            _t.startCharCode = this.stream.getUint32();
+            _t.endCharCode = this.stream.getUint32();
+            _t.glyphID = this.stream.getUint32();
+
+            _vals.push( _t );
+        }
+
+        return _vals;
+    }
+
+    parser () {
+        var _self = this;
+
+        this.version = this.stream.getUint16();
+        this.numberSubtables = this.stream.getUint16();
+
+        this.encodingTables = [];
+
+        for ( let i = 0; i < this.numberSubtables; i++ ) {
+            var _enc = {};
+            _enc.platformID = this.stream.getUint16();
+            _enc.platformSpecificID = this.stream.getUint16();
+            _enc.offset = this.stream.getUint32();
+
+            this.encodingTables.push( _enc );
+        }
+
+        this.formats = [];
+
+        for ( let i = 0; i < this.numberSubtables; i++ ) {
+            let _format = {};
+            this.stream.__pointer = this.encodingTables[i].offset;
+
+            _format.format = this.stream.getUint16();
+
+            let _switch = {
+                0 : function() {
+                    _format.length = _self.stream.getUint16();
+                    _format.language = _self.stream.getUint16();
+
+                    _format.glyphIndexArray = _self.stream.getUint8Array( 256 );
+                },
+                // TODO:
+                2 : function() {
+                    _format.length = _self.stream.getUint16();
+                    _format.language = _self.stream.getUint16();
+                    // _format.subHeaderKeys = this.stream.getUint8Array( 256 );
+                },
+                4 : function() {
+                    _format.length = _self.stream.getUint16();
+                    _format.language = _self.stream.getUint16();
+
+                    _format.segCountx2 = _self.stream.getUint16();
+
+                    var _segCount = _format.segCountx2 >> 1;
+
+                    _format.searchRange = _self.stream.getUint16();
+                    _format.entrySelector = _self.stream.getUint16();
+                    _format.rangeShift = _self.stream.getUint16();
+                    _format.endCount = _self.stream.getUint16Array( _segCount );
+                    _format.reservedPad = _self.stream.getUint16();
+                    _format.startCount = _self.stream.getUint16Array( _segCount );
+                    _format.idDelta = _self.stream.getInt16Array( _segCount );
+                    _format.idRangeOffset = _self.stream.getUint16Array( _segCount );
+
+                    _format.glyphIndexArray = _self.stream.getUint16Array( ( _format.length - ( 16 + ( _segCount << 3 ) ) ) >> 1 );
+                },
+                6 : function() {
+                    _format.length = _self.stream.getUint16();
+                    _format.language = _self.stream.getUint16();
+
+                    _format.firstCode = _self.stream.getUint16();
+                    _format.entryCode = _self.stream.getUint16();
+
+                    _format.glyphIndexArray = _self.stream.getUint16Array( _format.entryCode );
+                },
+                8 : function() {
+                    _format.reserved = _self.stream.getUint16();
+                    _format.length = _self.stream.getUint32();
+                    _format.language = _self.stream.getUint32();
+
+                    _format.is32 = _self.stream.getUint8Array( 8192 );
+                    _format.numGroups = _self.stream.getUint32();
+
+                    _format.SequentialMapGroup = _self._sequentialMap( _format.numGroups );
+                },
+                10 : function() {
+                    _format.reserved = _self.stream.getUint16();
+                    _format.length = _self.stream.getUint32();
+                    _format.language = _self.stream.getUint32();
+
+                    _format.startCharCode = _self.stream.getUint32();
+                    _format.numGroups = _self.stream.getUint32();
+                    _format.glyphIndexArray = _self.stream.getUint16Array( ( _format.length - 18 ) >> 1 );
+                },
+                12 : function() {
+                    _format.reserved = _self.stream.getUint16();
+                    _format.length = _self.stream.getUint32();
+                    _format.language = _self.stream.getUint32();
+                    _format.numGroups = _self.stream.getUint32();
+
+                    _format.SequentialMapGroup = _self._sequentialMap( _format.numGroups );
+                },
+                13 : function() {
+                    _format.reserved = _self.stream.getUint16();
+                    _format.length = _self.stream.getUint32();
+                    _format.language = _self.stream.getUint32();
+                    _format.numGroups = _self.stream.getUint32();
+
+                    _format.ConstantMapGroup = _self._constantMap( _format.numGroups );
+                },
+                // TODO:
+                14 : function() {},
+            };
+
+            _switch[ _format.format ]();
+
+            this.formats.push( _format );
+        }
+
+        console.log( this );
+    }
+}
+
+class head extends sfnt {
+    parser () {
+        var _self = this;
+
+        this.majorVersion = this.stream.getUint16();
+        this.minorVersion = this.stream.getUint16();
+
+        this.fontRevison = this.stream.getFixed32();
+
+        console.log( this );
     }
 }
 
@@ -82,7 +287,27 @@ class Fonts {
     }
 
     static table_name () {
-        return [ "acnt", "ankr", "avar", "bdat", "bhed", "bloc", "bsln", "cmap", "cvar", "cvt ", "EBSC", "fdsc", "feat", "fmtx", "fond", "fpgm", "fvar", "gasp", "gcid", "glyf", "gvar", "hdmx", "head", "hhea", "hmtx", "just", "kern", "herx", "lcar", "loca", "ltag", "maxp", "meta", "mort", "morx", "name", "opbd", "OS/2", "post", "prep", "prop", "sbix", "trak", "vhea", "vmtx", "xref", "Zapf" ];
+        return [
+            // required tables
+            "cmap", "head", "hhea", "hmtx", "maxp", "name", "OS/2", "post",
+            // TureType outlines
+            "cvt ", "fpgm", "glyf", "loca", "prep", "gasp",
+            // PostScript outlines
+            "CFF ", "CFF2", "VORG",
+            // SVG outlines
+            "SVG ",
+            // Bitmap glyps
+            "EBDT", "EBLC", "EBSC", "CBDT", "CBLC", "sbix",
+            // typographic tables
+            "BASE", "GDEF", "GPOS", "GSUB", "JSTF", "MATH",
+            // OpenType variations
+            "avar", "cvar", "fvar", "gvar", "HVAR", "MVAR", "VVAR",
+            // color fonts
+            "COLR", "CPAL", "CBDT", "CBLC", "sbix", "SVG ",
+            // OpenType tables
+            "DSIG", "hdmx", "kern", "LTSH", "MERG", "meta", "STAT", "PCLT", "VDMX", "vhea", "vmtx"
+        ];
+        // return [ "acnt", "ankr", "avar", "bdat", "bhed", "bloc", "bsln", "cmap", "cvar", "cvt ", "EBSC", "fdsc", "feat", "fmtx", "fond", "fpgm", "fvar", "gasp", "gcid", "glyf", "gvar", "hdmx", "head", "hhea", "hmtx", "just", "kern", "herx", "lcar", "loca", "ltag", "maxp", "meta", "mort", "morx", "name", "opbd", "OS/2", "post", "prep", "prop", "sbix", "trak", "vhea", "vmtx", "xref", "Zapf" ];
     }
 
     _get_sfnt ( meta ) {
@@ -94,43 +319,10 @@ class Fonts {
 
     _parser_cmap () {
         var cmap_stream = this._get_sfnt( "cmap" );
+        this.cmap = new cmap( cmap_stream );
 
-        this.cmap = {};
-
-        this.cmap.version = cmap_stream.getUint16();
-        this.cmap.numberSubtables = cmap_stream.getUint16();
-
-        this.cmap.encodingTables = [];
-
-        for ( let i = 0; i < this.cmap.numberSubtables; i++ ) {
-            var _enc = {};
-            _enc.platformID = cmap_stream.getUint16();
-            _enc.platformSpecificID = cmap_stream.getUint16();
-            _enc.offset = cmap_stream.getUint32();
-
-            this.cmap.encodingTables.push( _enc );
-        }
-
-        for ( let i = 0; i < this.cmap.numberSubtables; i++ ) {
-            var _formats = {};
-            cmap_stream.__pointer = this.cmap.encodingTables[i].offset;
-
-            _formats.format = cmap_stream.getUint16();
-
-            console.log( _formats.format );
-        }
-
-/*
-        var _t = {};
-        _t.format = this.stream.getUint16();
-        _t.length = this.stream.getUint16();
-        _t.language = this.stream.getUint16();
-        _t.glyphIndexArray = this.stream.getUint8Array( _t.length - 6 );
-
-        this.cmap.subTables.push( _t );
-        */
-
-        console.log( this.cmap );
+        var head_stream = this._get_sfnt( "head" );
+        this.head = new head( head_stream );
     }
 
     parser () {
--- src/tests/fonts/fonts.js
+++ src/tests/fonts/fonts.js
@@ -2,5 +2,14 @@
 
 importScripts( "../../lib/fonts/fonts.js" );
 
-var f = new Fonts( "../../fonts/BahiaScriptSSK.ttf", function( data ) {
+// cmap 2 4 2
+// var f = new Fonts( "../../fonts/BahiaScriptSSK.ttf", function( data ) {
+// cmap 4
+// var f = new Fonts( "../../fonts/我字酷默陌写意水墨体.ttf", function( data ) {
+// cmap 4 6 4
+// var f = new Fonts( "../../fonts/Switzerl.ttf", function( data ) {
+// cmap 4 4 12
+var f = new Fonts( "../../fonts/msyh.ttf", function( data ) {
+// cmap 4 12 6 4 12
+// var f = new Fonts( "../../fonts/NotoSansHans-Black.otf", function( data ) {
 } );
